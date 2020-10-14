8f3f11d65f855542e77fd844cfe7d9eb098ce82e
a4327481 --global user.name a4327481 <a4327481@126.com>
2020-10-14 17:57:45 +0800

    day 20201014

--- to_sl/Compter_Plane_inint_m_V10.m
+++ to_sl/Compter_Plane_inint_m_V10.m
@@ -3,7 +3,7 @@ load IOBusInfo_byc_20200903.mat
 load IOBusInfo_V1000.mat
 pianzhuanjiao
 quad_tail_4a1=0;
-plane_mode=ENUM_plane_mode.V10;
+plane_mode=ENUM_plane_mode.V1000;
 
 m_kg_V1000=5;
 Jx=186222*1e-6;
@@ -59,7 +59,7 @@ vel_forward_integrator=0;
 PathModeOut_sl.headingCmd=0;
 PathModeOut_sl.groundspeedCmd=0;
 PathModeOut_sl.heightCmd=10000;
-PathModeOut_sl.flightTaskMode=ENUM_FlightTaskMode.HoverAdjustMode;
+PathModeOut_sl.flightTaskMode=ENUM_FlightTaskMode.Rotor2Fix_Mode;
 PathModeOut_sl.flightControlMode=ENUM_FlightControlMode.SpotHoverMode;
 PathModeOut_sl.maxClimbSpeed=100;
 PathModeOut_sl.turnCenterLL=[40,100]*1e7;
@@ -190,6 +190,8 @@ POSCONTROL_THROTTLE_CUTOFF_FREQ_p2c=0.2;
     k_elevator=0;
     POSCONTROL_ACC_Z_FILT_HZ_c2p=1;
      pitch_target_c2p                       = 0                         ;
+     Fix2Rotor_delay_s=0.8;
+     p_k_elevator_c2p=1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% wind %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%5    
 %      @Param: WVANE_MINROLL
